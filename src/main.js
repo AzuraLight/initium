e0b0a83a7a6aae1dbb9f4e9cfd9f50f9d348f398
AzuraLight <azuralight09@gmail.com>
2024-11-08 17:45:43 +0900

    popperjs 추가, babel 최적화 추가

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import router from '@/routes/route';
-import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
-
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 const app = createApp(App);
 
